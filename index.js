dc43fed3ee51345a33323fb312e0cf2fc275bb16
mohammadalmoqdad <almekad.mohammad@gmail.com>
2020-12-28 07:46:25 +0200

    all completed

---
+++ index.js
@@ -0,0 +1,7 @@
+
+let Input =require('./lib/input');
+const Note = require('./lib/notes');
+let note=require('./lib/notes')
+let newInput=new Input();
+let newNote=new Note(newInput);
+// console.log("CONSTRUCOOOOOOR IN indes.js",newInput);
