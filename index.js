1da26af6863b25e5708fee3764d873e992ae730f
mohammadalmoqdad <almekad.mohammad@gmail.com>
2020-12-30 07:53:05 +0200

    lab solved but test not yet

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
 
 let Input =require('./lib/input');
 const Note = require('./lib/notes');
-let note=require('./lib/notes')
+let mongoose=require('mongoose');
+require('dotenv').config();
+const MONGO_URL=process.env.MONGO_URL;
+
+
+mongoose.connect(MONGO_URL,{
+    //to avoid warnings
+    useNewUrlParser:true,
+    useUnifiedTopology:true,
+    useCreateIndex:true,
+    useFindAndModify:false
+})
+
+
 let newInput=new Input();
 let newNote=new Note(newInput);
+
+
+// mongoose.disconnect();
+
 // console.log("CONSTRUCOOOOOOR IN indes.js",newInput);
